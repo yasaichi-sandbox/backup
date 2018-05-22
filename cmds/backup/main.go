c5e36d7a2cbff178f3463ddf9ce212f9aa5bd673
yasaichi <yasaichi@users.noreply.github.com>
2018-05-22 22:01:21 +0900

    8.3.1

--- cmds/backup/main.go
+++ cmds/backup/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"errors"
 	"flag"
+	"github.com/matryer/filedb"
 	"log"
 )
 
+type path struct {
+	Path string
+	Hash string
+}
+
 func main() {
 	var fatalErr error
-
 	defer func() {
 		if fatalErr != nil {
 			flag.PrintDefaults()
@@ -17,11 +22,24 @@ func main() {
 	}()
 
 	dbpath := flag.String("db", "./backupdata", "データベースのディレクトリへのパス")
-	_ = dbpath
 
 	flag.Parse()
 	if len(flag.Args()) < 1 { // NOTE: `Args` returns the non-flag arguments.
 		fatalErr = errors.New("エラー; コマンドを指定してください")
+		return
+	}
+
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+
+	col, err := db.C("paths")
+	_ = col
+	if err != nil {
+		fatalErr = err
 		return
 	}
 }
