03d7cbb961023bddf0f80f10644b02891e2b966a
yasaichi <yasaichi@users.noreply.github.com>
2018-05-23 23:00:32 +0900

    8.3.2.1

--- cmds/backup/main.go
+++ cmds/backup/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
+	"fmt"
 	"github.com/matryer/filedb"
 	"log"
 	"strings"
@@ -11,6 +13,10 @@ import (
 type path struct {
 	Path string
 	Hash string
+}
+
+func (p path) String() string {
+	return fmt.Sprintf("%s [%s]", p.Path, p.Hash)
 }
 
 func main() {
@@ -47,6 +53,18 @@ func main() {
 
 	switch strings.ToLower(args[0]) {
 	case "list":
+		var path path
+
+		col.ForEach(func(i int, data []byte) bool {
+			err := json.Unmarshal(data, &path)
+			if err != nil {
+				fatalErr = err
+				return true
+			}
+
+			fmt.Printf("= %s\n", path)
+			return false
+		})
 	case "add":
 	case "remove":
 	}
